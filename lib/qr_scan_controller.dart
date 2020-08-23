d70e535775b21335585e0e95c45e51f04423d871
wshunli <wshunli@qq.com>
2020-08-23 09:52:05 +0800

    feat: 优化原生方法调用

--- lib/qr_scan_controller.dart
+++ lib/qr_scan_controller.dart
@@ -6,57 +6,57 @@ class QrScanController {
   QrScanController(this._channel);
 
   /// 显示扫描框
-  void showScanRect() {
+  Future<void> showScanRect() async {
     _channel.invokeMethod("showScanRect");
   }
 
   /// 隐藏扫描框
-  void hiddenScanRect() {
+  Future<void> hiddenScanRect() async {
     _channel.invokeMethod("hiddenScanRect");
   }
 
   /// 打开后置摄像头开始预览，但是并未开始识别
-  void startCamera() {
+  Future<void> startCamera() async {
     _channel.invokeMethod("startCamera");
   }
 
   /// 关闭摄像头预览，并且隐藏扫描框
-  void stopCamera() {
+  Future<void> stopCamera() async {
     _channel.invokeMethod("stopCamera");
   }
 
   /// 开始识别
-  void startSpot() {
+  Future<void> startSpot() async {
     _channel.invokeMethod("startSpot");
   }
 
   /// 显示扫描框，并开始识别
-  void startSpotAndShowRect() {
+  Future<void> startSpotAndShowRect() async {
     _channel.invokeMethod("startSpotAndShowRect");
   }
 
   /// 停止识别
-  void stopSpot() {
+  Future<void> stopSpot() async {
     _channel.invokeMethod("stopSpot");
   }
 
   /// 停止识别，并且隐藏扫描框
-  void stopSpotAndHiddenRect() {
+  Future<void> stopSpotAndHiddenRect() async {
     _channel.invokeMethod("stopSpotAndHiddenRect");
   }
 
   /// 打开闪光灯
-  void openFlashlight() {
+  Future<void> openFlashlight() async {
     _channel.invokeMethod("openFlashlight");
   }
 
   /// 关闭闪光灯
-  void closeFlashlight() {
+  Future<void> closeFlashlight() async {
     _channel.invokeMethod("closeFlashlight");
   }
 
   /// 解析本地图片二维码。返回二维码图片里的内容 或 null
-  void decodeQRCode(String picturePath) {
+  Future<void> decodeQRCode(String picturePath) async {
     _channel.invokeMethod("decodeQRCode", picturePath);
   }
 }
